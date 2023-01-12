b42b78f27b5da7609d9517e5d9655282cff87194
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2023-01-12 21:27:20 +0000

    Playing around with DOM

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 'use strict';
 
-document.querySelector('.message').textContent;
+document.querySelector('.message').textContent = 'Test';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 4444;
