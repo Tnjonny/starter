ac821e6245a1ec7064777a065648cabd9598f1a9
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2023-01-13 23:58:16 +0000

    Removed test DOM selectors

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
 'use strict';
-
-document.querySelector('.message').textContent = 'Test';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 4444;
